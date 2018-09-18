4c8b31438b5b41ba794b803b294526bbd75772ca
ram5shh <ramesh18.r@gmail.com>
2018-09-18 09:15:14 -0500

    Update GuessNumberGame.py

--- GuessNumberGame.py
+++ GuessNumberGame.py
@@ -1,22 +1,61 @@
-import random                  
-rng = random.Random()          
-number = rng.randrange(1, 11) #get random number between 1 and 10
+# Your friend will complete this function
 
-guesses = 0 #count the number of guesses
+# Add logic so that the players take turns to play first. - YTD
 
-while True:
-    input_from_user = int(input("Guess the number between 1 and 10: (enter 0 to exit) "))
-    guesses += 1
-    if input_from_user <1 or input_from_user >10:
-        print("Thanks for playing")
-        break
-    if input_from_user > number:
-        print("Too high")
-    elif input_from_user <number:
-        print("Too low")
+def play_once(human_plays_first):
+    """
+       Must play one round of the game. If the parameter
+       is True, the human gets to play first, else the
+       computer gets to play first.  When the round ends,
+       the return value of the function is one of
+       -1 (human wins),  0 (game drawn),   1 (computer wins).
+    """
+    # This is all dummy scaffolding code right at the moment...
+    import random                  # See Modules chapter ...
+    rng = random.Random()
+    # Pick a random result between -1 and 1.
+    result = rng.randrange(-1,2)
+    print("Human plays first={0},  winner={1} "
+                       .format(human_plays_first, result))
+    return result
+
+#Write the main program which repeatedly calls this function to play the game,
+# and after each round it announces the outcome as “I win!”, “You win!”, or “Game drawn!”.
+# It then asks the player “Do you want to play again?” and either plays again, or says “Goodbye”, and terminates.
+
+def game():
+    counter_player = 0 # Keep score of how many wins each player has had, and how many draws there have been. After each round of play, also announce the scores.
+    counter_comp = 0
+    counter_draws = 0
+
+    total_games_played =0 # Compute the percentage of wins for the human, out of all games played. Also announce this at the end of each round.
+
+    wannaPlay = input("Do you want to play a game? ")
+    while wannaPlay == "yes":
+        total_games_played = total_games_played+1
+        n = int(input("Enter your value: -1, 0, 1: "))
+        a = play_once(n)
+        if a == -1:
+            print("You Win.", "\n")
+            counter_player = counter_player+1
+        elif a == 0:
+            print("Game Drawn.", "\n")
+            counter_draws += 1
+        elif a == 1:
+            print("I Win.", "\n")
+            counter_comp += 1
+
+        print("You won: ", counter_player, "times")
+        print("I won: ", counter_comp, "times")
+        print("Game drawn: ", counter_draws, "times \n")
+
+        winPercent_1 = (counter_player/total_games_played) *100
+        winPercent = round(winPercent_1,2)
+
+        print("Hooman win percent after this round", winPercent)
+
+        wannaPlay = input("Do you want to play again?")
     else:
-        break
+        print("Good Bye")
 
-if input_from_user >0 and input_from_user <11:
-    print("It is a match: ", input_from_user, "Number of guesses", guesses)
-    
+game()
